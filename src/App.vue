4eda06e11a780d32fbdbe9d61e3502362b885ad8
Yeojinny <pdg03092@gmail.com>
2022-05-03 21:09:40 +0900

    merge yeojin branch

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <v-app id="app">
-    <router-view></router-view>
+  <v-app>
+    <Header />
+    <router-view />
+    <Footer />
   </v-app>
 </template>
 
 <script>
+import Header from "@/components/common/Header.vue";
+import Footer from "@/components/common/Footer.vue";
 export default {
   name: "App",
+  components: {
+    Header,
+    Footer,
+  },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style></style>
