311244b5d8ed67839904752c6dce62a23aab5cfc
Yeojinny <pdg03092@gmail.com>
2022-05-03 21:43:01 +0900

    push test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router/index";
+import { router } from "./router/index";
 import { store } from "./store/store.js";
 import axios from "axios";
 import vuetify from "./plugins/vuetify";
@@ -9,8 +9,8 @@ Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
 
 new Vue({
-  vuetify,
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
